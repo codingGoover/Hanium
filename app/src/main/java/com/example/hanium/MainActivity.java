937c8a2c0673d1d58b3e378ac3eda6f1a96cfd11
codingGoover <dpswpfgkxm30@naver.com>
2021-08-02 16:54:43 +0900

    roundbutton

--- app/src/main/java/com/example/hanium/MainActivity.java
+++ app/src/main/java/com/example/hanium/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Departure_car);
+        setContentView(R.layout.departure_car);
     }
 }
