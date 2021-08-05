9beb863fb7b4852a90f390e8a64dad445bb2b759
codingGoover <dpswpfgkxm30@naver.com>
2021-08-05 15:24:43 +0900

    switch page

--- app/src/main/java/com/example/hanium/HomeActivity.java
+++ app/src/main/java/com/example/hanium/HomeActivity.java
@@ -1,10 +1,19 @@
 package com.example.hanium;
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity{
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+    }
+
+    public void onClick(View v){
+        switch(v.getId())
+        {
+        }
     }
 }
--- app/src/main/java/com/example/hanium/SignUpActivity.java
+++ app/src/main/java/com/example/hanium/SignUpActivity.java
@@ -1,10 +1,32 @@
 package com.example.hanium;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    Button signBT;
+    Intent intent;
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+        signBT=(Button)findViewById(R.id.button3);
+        signBT.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+           case R.id.button3:
+                //회원가입 버튼 클릭시 일단 로그인 페이지로 이동 (실제: id,pw 가입필요)
+                intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+
 }
